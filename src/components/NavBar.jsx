0e1f9a87d2677ba8c741bf2082d6268a0737fe62
AK47-BU <s5721202@bournemouth.ac.uk>
2025-10-23 01:05:00 +0100

    Completed navbar, fixed CSS and Jsx issues, resolving alignmet and sizing issues

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useState, useEffect } from 'react';
-import logo from "../assets/img/saul.jpg"
+import logo from "../assets/img/IMG_4938.jpeg"
 import navIcon1 from "../assets/img/nav-icon1.svg"
 import navIcon3 from "../assets/img/nav-icon3.svg"
 import navIcon2 from "../assets/img/github-brands-solid-full.svg"
@@ -34,10 +34,11 @@ import navIcon2 from "../assets/img/github-brands-solid-full.svg"
         <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
         <Container>
             <Navbar.Brand href="#home">
-                <image src={logo} alt="Logo" />
+                <img src={logo} alt="Logo" />
             </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Toggle aria-controls="basic-navbar-nav">
                 <span className='navbar-toggle-icon'></span>
+            </Navbar.Toggle>
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
                 <Nav.Link href="#home" className={activelink === `home` ? `active navbar-link` : `navbar-link`} onClick={() => onUpdateActiveLink(`home`)}>Home</Nav.Link>
@@ -45,16 +46,18 @@ import navIcon2 from "../assets/img/github-brands-solid-full.svg"
                 <Nav.Link href="#Projects" className={activelink === `Projects` ? `active navbar-link` : `navbar-link`} onClick={() => onUpdateActiveLink(`Projects`)}>Projects</Nav.Link>               
             </Nav>
             <span className='navbar-text'>
-                <div className='social icon'>
+                <div className='social-icon'>
                 <Nav.Link href="#Instagram"><img src={navIcon1} alt="#Instagram"/></Nav.Link>
                 <Nav.Link href="#LinkedIn"><img src={navIcon3} alt="#LinkedIn"/></Nav.Link>
                 <Nav.Link href="#Github"><img src={navIcon2} alt="#Github"/></Nav.Link>
                 </div>
-                <button className='contact' onClick={() => console.log(`connect`)}></button><span>Let's connect</span>
+                <button className='contact' onClick={() => console.log(`connect`)}>
+                <span>Let's connect</span>
+                </button>
                 
             </span>
             </Navbar.Collapse>
-        </Container>
+            </Container>
         </Navbar>
     );
-    }
+}
